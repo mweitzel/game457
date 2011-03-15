6c149bdd7f7d72f041118b3cc92d89c79e390c4b
Matthew Weitzel <mweitzel@gmail.com>
2011-03-15 18:28:55 -0400

    fixed bug where health would increase passed maximum

--- Assets/Our Assets/Mergeable/JavaScript/DamageLogic/Stats.js
+++ Assets/Our Assets/Mergeable/JavaScript/DamageLogic/Stats.js
@@ -9,6 +9,7 @@
 var attack = 1;
 var defense = 1;
 var health = 1;
+var maxHealth = 1;
 var invincible  = false;
 var isPlayer = false;
 var eleType = "WATER";
@@ -26,8 +27,8 @@ function destroyObjectIfPossible(){
 function updateHealthBar(){
 	if(isPlayer == true) {
 		var hpBar : PlayerHealth;
-		hpBar = this.gameObject.GetComponent("PlayerHealth");
-		hpBar. SetCurrentHealth(health);
+//		hpBar = this.gameObject.GetComponent("PlayerHealth");
+//		hpBar. SetCurrentHealth(health);
 	}
 }
 
@@ -37,6 +38,9 @@ function shouldDie() {
 
 function DecreaseHealth(amount){
 	health -= amount;	
+	
+	if(health > maxHealth)
+		health = maxHealth;
 }
 
 
--- Assets/Our Assets/Mergeable/JavaScript/GUI/PlayerHealth.js
+++ Assets/Our Assets/Mergeable/JavaScript/GUI/PlayerHealth.js
@@ -9,103 +9,13 @@
 /// Oct 20, 2010
 /// Peter Laliberte
 
-
-var maxHealth = 1000;
-var curHealth = 1000;
-var healthBarLength = 0;
-
-function Update () {
-		AddjustCurrentHealth(0);
-}
-
-// use for initialization
-function Start() {
+	var characterStats : Stats;
+	var healthBarLength = 0;
+	
+	function Start() {
 		healthBarLength = Screen.width /4;
 	}
-	//Update is called once pre frame
-	function update() {
-		//AddjustCurrentHealth(0);
-		OnGUI();
-	}
+		
 	function OnGUI() {
-		 GUI.Box(Rect(10,10,healthBarLength, 20), curHealth + "/" + maxHealth);
+		 GUI.Box(Rect(10,10,healthBarLength, 20), characterStats.health + "/" + characterStats.maxHealth);
 	}
-	function SetCurrentHealth(adj: int) {
-	  		curHealth = adj;
-  		
-  		if(curHealth < 0){
-  			curHealth = adj;
-			}
-  		
-  		if(curHealth > maxHealth)
-  			curHealth = maxHealth;
-  		
- 		if(maxHealth < 1)
- 			maxHealth = 1;
-
- 		//healthBarLength = (Screen.width / 2);
- 	}
-	function AddjustCurrentHealth(adj: int) {
-  		curHealth += adj;
-  		  		
-  		if(curHealth > maxHealth)
-  			curHealth = maxHealth;
-  		
- 		if(maxHealth < 1)
- 			maxHealth = 1;
-
- 		//healthBarLength = (Screen.width / 2);
- 	}
-
-/////////////////////////////////////////////////////////////////////////
-/// <summary>
-/// PlayerHealth.cs
-/// Oct 20, 2010
-/// Peter Laliberte
-/// 
-/// Modified By Calvin Slusarski
-/// February 19 2011
-/// Display the players health in game
-/// 
-/// Attach this class to your player character
-/// </summary>
-/* using UnityEngine;
- * using System.Collections;
- * 
- * public class PlayerHealth : MonoBehaviour {
- * 	public int maxHealth = 100;
- * 	public int curHealth = 100;
- * 	// Added By Calvin: Adjust colors via inspector
- * 	//public GUIStyle newStyles = new  GUIStyle();
- * 	
- * 	public float healthBarLength;
- * 
- * 	// Use this for initialization
- * 	void Start () {
- * 		healthBarLength = Screen.width / 2;
- * 	}
- * 	
- * 	// Update is called once per frame
- * 	void Update () {
- * 		AddjustCurrentHealth(0);
- * 	}
- * 	
- * 	void OnGUI() {
- * 		GUI.Box(new Rect(10, 10, healthBarLength, 20), curHealth + "/" + maxHealth/*, newStyles*//*);
- * 	}
- * 	
- * 	public void AddjustCurrentHealth(int adj) {
- * 		curHealth += adj;
- * 		
- * 		if(curHealth < 0)
- * 			curHealth = 0;
- * 		
- * 		if(curHealth > maxHealth)
- * 			curHealth = maxHealth;
- * 		
- * 		if(maxHealth < 1)
- * 			maxHealth = 1;
- * 		
- * 		healthBarLength = (Screen.width / 2) * (curHealth / (float)maxHealth);
- * 	}
- * } */
