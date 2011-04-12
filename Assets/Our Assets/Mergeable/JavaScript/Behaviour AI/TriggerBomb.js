a2c0e2f497757f087360c816a31cfb2807d70ed2
Matthew Weitzel <mweitzel@gmail.com>
2011-04-12 14:27:07 -0400

    cleaned up first level

--- Assets/Our Assets/Mergeable/JavaScript/Behaviour AI/TriggerBomb.js
+++ Assets/Our Assets/Mergeable/JavaScript/Behaviour AI/TriggerBomb.js
@@ -1,4 +1,5 @@
 var explosionStrength = 10;
+var delayTime = 1;
 var bomb : Stats[];
 
 
@@ -10,7 +11,7 @@ function respond() {
 	whichType = Random.Range(0,bomb.length);
 	whichType = Mathf.Floor(whichType);
 	
-	yield WaitForSeconds (1);
+	yield WaitForSeconds (delayTime);
 	positionToMake = gameObject.transform.position;
 	clone = Instantiate(bomb[whichType], positionToMake, Quaternion.identity);
 	clone.GetComponent(Stats).attack = explosionStrength;
